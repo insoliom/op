3677db6334412f3f2d19651eb2863c9e2094d3ff
Egor Rybkin <114059794+insoliom@users.noreply.github.com>
2022-11-13 22:45:07 +0200

    third hw(Pascal Triangle)

---
+++ Program5.cs
@@ -0,0 +1,32 @@
+﻿
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Enter pls the number");
+        int rows = Convert.ToInt32(Console.ReadLine());
+        int res = 1;
+        int x;
+        int i; // number of column
+        int j; // number of elements
+
+        for (i = 0; i < rows; i++)
+        {
+            for (x = 1; c <= rows - i; x++)
+            {
+                Console.Write(" "); // spaces
+            }
+            for (j = 0; j <= i; j++)
+            {
+                if (j == 0 || i == 0 || j == i)
+                {
+                    res = 1;
+                }
+                else
+                {
+                    res = res * (i - j + 1) / j;
+                }
+                Console.Write(" " + res);
+            }
+            Console.WriteLine();
+        }
