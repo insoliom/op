517a0cd0d23b105e2ce529b674cec01d24a71c36
Egor Rybkin <114059794+insoliom@users.noreply.github.com>
2023-01-14 08:48:23 +0200

    Update Program2.cs

--- Program2.cs
+++ Program2.cs
@@ -1,45 +1,59 @@
-﻿class Program
+class Program
 {
     public static void Main(string[] args)
     {
-        /* Тестові сценерії:
-             1: n = 0;
-             2: n = 1; 
-             3: n = 5;
-             4: n = -1;              */
-        bool prime = true;
-        Console.Write("Enter Number ");
-        int n = int.Parse(Console.ReadLine());
-        if (n < 0)
+        
+        while (true)
         {
-            Console.WriteLine("negative cant be prime");
-            return;
-        }
-        for (int i = 2; i <= n / 2 + 2; i++)
-        {
-            if (n % i == 0 || n == 1)
+            bool prime = true;
+            Console.Write("Enter Number ");
+            int n = int.Parse(Console.ReadLine());
+            if (n < 0)
             {
-                prime = false;
+                Console.WriteLine("negative cant be prime");
+                return;
+            }
+            for (int i = 2; i <= n / 2 + 2; i++)
+            {
+                if (n % i == 0 || n == 1 || n == 2)
+                {
+                    prime = false;
 
+                }
+                else
+                {
+                    prime = true;
+                }
+                break;
+            }
+            if (prime)
+            {
+                Console.WriteLine("This is prime number");
             }
             else
             {
-                prime = true;
+                Console.WriteLine("This is not prime number");
             }
-            break;
+            /* Enter Number 0
+               This is not prime number
+
+               Enter Number 1
+               This is not prime number
+
+               Enter Number 2
+               This is not prime number
+
+               Enter Number 3
+              This is prime number
+
+               Enter Number 4
+               This is not prime number
+               Enter Number 5
+
+               This is prime number
+
+            */
         }
-        if (prime)
-        {
-            Console.WriteLine("This is prime number");
-        }
-        else
-        {
-            Console.WriteLine("This is not prime number");
-        }
-        /* Тестові сценерії:
-            1:This is not prime number ;
-            2:This is not prime number; ;
-            3:This is prime number;  
-            4:negative cant be prime 
-        */
     }
+            
+}
