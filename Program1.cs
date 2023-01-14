bcefa6e57b50f7e8700a723bded3cc2ff75426c0
Egor Rybkin <114059794+insoliom@users.noreply.github.com>
2023-01-14 08:40:56 +0200

    Update Program1.cs

--- Program1.cs
+++ Program1.cs
@@ -13,7 +13,7 @@
         double n = Convert.ToDouble(Console.ReadLine());
         double x0 = 1.23;
         double b = 12.6;
-        for (int i = 0; i <= n; i++)
+        for (int i = 0; i < n; i++)
         {
             double x = x0 + i * (xn - x0) / n;
             Console.WriteLine("x= " + Math.Round(x, 2));
@@ -24,11 +24,12 @@
 
         /* Тестові сценарії:
          1: x= не число; y= не число 
+         
          2: x= 1,23; y= 13,39
-            x= 1; y= 16,28
+           
          3: x= 1,23; y= 13,39
             x= 2,49; y= 6,73
             x= 3,74; y= 4,3
-            x= 5; y= 2,98     */
+              */
     }
 }
