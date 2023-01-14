07ce46340f7785104f8d98352e4e43e90acd9cdf
Egor Rybkin <114059794+insoliom@users.noreply.github.com>
2023-01-14 09:19:43 +0200

    Update Program2.cs

--- Program2.cs
+++ Program2.cs
@@ -1,59 +1,59 @@
 class Program
 {
-    public static void Main(string[] args)
-    {
-        
-        while (true)
-        {
-            bool prime = true;
-            Console.Write("Enter Number ");
-            int n = int.Parse(Console.ReadLine());
-            if (n < 0)
-            {
-                Console.WriteLine("negative cant be prime");
-                return;
-            }
-            for (int i = 2; i <= n / 2 + 2; i++)
-            {
-                if (n % i == 0 || n == 1 || n == 2)
-                {
-                    prime = false;
 
-                }
-                else
-                {
-                    prime = true;
-                }
-                break;
-            }
-            if (prime)
+    public static void Main(string[] args)
+
+    {
+         
+            Console.WriteLine("Enter num");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n <0)
             {
-                Console.WriteLine("This is prime number");
+               Console.WriteLine("we cant use negative\n");
+            return;
+
+            }
+            if (n == 0 || n == 1)
+            {
+                Console.WriteLine( "is not prime number\n");
+                
             }
             else
             {
-                Console.WriteLine("This is not prime number");
+                for (int a = 2; a <= n / 2; a++)
+                {
+                    if (n % a == 0)
+                    {
+                        Console.WriteLine("is not prime number\n");
+                        return;
+                    }
+
+                }
+                Console.WriteLine("is  prime number\n");
+
             }
-            /* Enter Number 0
-               This is not prime number
+            /*   Enter num
+                 0
+                 is not prime number
 
-               Enter Number 1
-               This is not prime number
+                 Enter num
+                 1
+                 is not prime number
 
-               Enter Number 2
-               This is not prime number
+                 Enter num
+                 2
+                 is prime number
 
-               Enter Number 3
-              This is prime number
+                 Enter num
+                 3
+                 is prime number
 
-               Enter Number 4
-               This is not prime number
-               Enter Number 5
-
-               This is prime number
-
-            */
+                 Enter num
+                 4
+                 is not prime number
+                 Enter num
+                -1
+                 we cant use negative*/
         }
-    }
-            
+
 }
