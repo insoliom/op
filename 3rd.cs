43dba6ff858755240cbb5c94871cd7150b4c3c0a
Egor Rybkin <114059794+insoliom@users.noreply.github.com>
2023-02-27 01:37:09 +0200

    3rd task

---
+++ 3rd.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using static System.Net.Mime.MediaTypeNames;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       // Replace input.txt with the path to your input file
+        string inputFilePath = "input.txt";
+        string[] words = File.ReadAllLines(inputFilePath);
+
+       // Sort the words alphabetically
+        words = words.OrderBy(w => w).ToArray();
+
+        //Replace output.txt with the path to your output file
+        string outputFilePath = "output.txt";
+        using (StreamWriter writer = new StreamWriter(outputFilePath))
+        {
+            foreach (string word in words)
+            {
+                writer.WriteLine(word);
+            }
+        }
+
+        Console.WriteLine("Done"!);
+    }
+}
