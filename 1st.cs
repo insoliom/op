985be563fda139f00fe76d08143edde9e3052d6e
Egor Rybkin <114059794+insoliom@users.noreply.github.com>
2023-02-27 01:34:54 +0200

    1st task

---
+++ 1st.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string fileName = "mytextfile.txt";
+
+        try
+        {
+            // Create the text file
+            StreamWriter writer = new StreamWriter(fileName);
+            writer.WriteLine("This is the first line.");
+            writer.WriteLine("This is the second line.");
+            writer.Close();
+
+            // Read the contents of the text file and display them on the screen
+            string[] lines = File.ReadAllLines(fileName);
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine("Error creating or reading file: " + e.Message);
+        }
+    }
+}
