35084caf543241954ed10ca7b26a2a4860259310
Egor Rybkin <114059794+insoliom@users.noreply.github.com>
2023-04-17 17:37:44 +0300

    2nd

---
+++ 2.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Polymorhism
+{
+    abstract class Vessel
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Vessel[] ves = new Vessel[]
+                {
+            new SailingVessel(),
+            new Submarine()
+                };
+                foreach (Vessel vessel in ves)
+                {
+                    vessel.PrepareToMoving();
+                }
+
+
+
+                foreach (Vessel vessel in ves)
+                {
+                    vessel.Move();
+                }
+            }
+        }
+        public abstract void PrepareToMoving();
+        public abstract void Move();
+
+
+        class SailingVessel : Vessel
+        {
+            public override void PrepareToMoving()
+            {
+                Console.WriteLine("SailingVessel is prepare to move");
+            }
+
+            public override void Move()
+            {
+                Console.WriteLine("SailingVessel is moving");
+            }
+        }
+
+        class Submarine : Vessel
+        {
+            public override void PrepareToMoving()
+            {
+                Console.WriteLine("Submarine is prepare to move");
+            }
+            public override void Move()
+            {
+                Console.WriteLine("Submarine is moving");
+            }
+        }
+
+
+
+
+    }
+}
+
+       
